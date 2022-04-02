62d6c3ebd1b9cb6a325964e884cf8dcf596009a7
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-02 11:06:23 -0400

    Commit import index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM  from "react";
+import './style.css';
+import App from './App';
+
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById('root')
+)
