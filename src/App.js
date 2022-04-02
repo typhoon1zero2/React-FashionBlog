de456c2c49bb9f6404988eaf3f36b15fdac85520
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-02 11:10:12 -0400

    Commit imports App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Article  from './components/Article';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+
+export default function App(){
+    return(
+        <div className='App'>
+            <Header />
+            <Nav />
+            <Article />
+            <Footer />
+        </div>
+    )
+}
