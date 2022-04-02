85b81a33a6fecf6f69ab33fae07a4f02f9829519
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-02 13:48:55 -0400

    Finished Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,54 @@
-export default function Article(){
-    return(
-        <section>
-            <article>
-                <time dateTime="11-12-20">11/12/21</time>
-                <div><h2>On the street in Brooklyn</h2></div>
-                
-                <div><img src="blog-image-1.jpeg" alt="Woman in pink dress" /></div>
-                    
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    </p>
-                </div>
-            </article>
-        </section>
-    )
+export default function Article() {
+  return (
+    <section>
+      <article className="article-1">
+        <time dateTime="11-12-20">11/12/21</time>
+        <div>
+          <h2>On the street in Brooklyn</h2>
+        </div>
+
+        <div>
+          <img src="blog-image-1.jpeg" alt="Woman in pink dress" />
+        </div>
+
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo
+          </p>
+        </div>
+        <a href=" " className="continues">
+          Continues ...
+        </a>
+      </article>
+      <article className="article-2">
+        <time dateTime="11-12-21">11/12/21</time>
+        <div>
+          <h2>Vintage in Vogue</h2>
+        </div>
+        <div>
+          <img src="blog-image-2.jpeg" alt="vintage-in-vogue" />
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            quibusdam cupiditate, deleniti debitis sit asperiores, temporibus
+            exercitationem consequuntur sequi eligendi ratione labore atque
+            commodi illum consequatur! Corrupti accusamus natus inventore? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut
+            debitis fuga ad veritatis, quibusdam dicta pariatur, a hic rerum
+            adipisci minima aperiam quaerat eaque tempore veniam. Debitis,
+            officiis veniam?
+          </p>
+        </div>
+        <a href=" " className="continues">Continues ...</a>
+      </article>
+    </section>
+  );
 }
